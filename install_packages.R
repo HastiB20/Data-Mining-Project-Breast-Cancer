f01e47ae3017c814a558e2481701db34535f2518
Hasti Bargharariyan <h.bmath10019@gmail.com>
2025-03-31 22:35:49 +0200

    Delete install_packages.R

--- install_packages.R
+++
@@ -1,4 +0,0 @@
-install.packages(c("data.table", "randomForest", "caret"), dependencies = TRUE)
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("EBImage")
